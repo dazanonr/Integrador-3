21724f1bb6a843a297abd256d8627c857f8a9496
dazanonr <dazanonr@indra.es>
2023-11-27 09:49:30 +0100

    Bucle

--- src/main/java/dazanonr/Main.java
+++ src/main/java/dazanonr/Main.java
@@ -1,19 +1,27 @@
 package dazanonr;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main (String[] args) throws InterruptedException {
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("¿Cuantos datos desea crear?");
+		int loop = scanner.nextInt();
 		Stock stock = new Stock();
 		View view = new View();
 		Controller controller = new Controller(view);
 
-		Thread mainThread = new Thread(new CreateStock(stock,controller));
-		Thread secondaryThread = new Thread(new CreateFinance(stock,controller));
+		for (int i = 0; i < loop; i++) {
+			Thread mainThread = new Thread(new CreateStock(stock, controller));
+			Thread secondaryThread = new Thread(new CreateFinance(stock, controller));
 
-		mainThread.start();
-		mainThread.join();
-		secondaryThread.start();
-		secondaryThread.join();
+			mainThread.start();
+			mainThread.join();
+			secondaryThread.start();
+			secondaryThread.join();
+		}
 	}
 }
